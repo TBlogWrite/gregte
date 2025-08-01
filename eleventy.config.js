569a8e347a509e0f5c26d9680b521f329d36116f
Tuan Tran Duong Ngoc <ittuan123@gmail.com>
2025-08-01 21:36:30 +0700

    Update

--- eleventy.config.js
+++ eleventy.config.js
@@ -1,6 +1,10 @@
 import { DateTime } from "luxon";
+import syntaxHighlight from "@11ty/eleventy-plugin-syntaxhighlight";
 
 export default function(eleventyConfig) {
+
+  eleventyConfig.addPlugin(syntaxHighlight);
+
   eleventyConfig.addFilter("date", (dateObj, format = "yyyy-MM-dd") => {
     return DateTime.fromJSDate(new Date(dateObj)).toFormat(format);
   });
